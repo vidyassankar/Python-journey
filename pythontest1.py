01e1e435aaf3f1e52f54342e7bd483f9d06bc106
Vidya <41564051+vidyassankar@users.noreply.github.com>
2020-08-19 22:09:46 +0530

    Update pythontest1.py

--- pythontest1.py
+++ pythontest1.py
@@ -1,14 +1,9 @@
-#print("press 1 for chrome: ")
-#print("press 2 for mediaplayer: ")
-#print("press 3 for notepad: ")
 
 import os
 
 while True:
 	print("Welcome ! , What are you looking for today? : ", end='')
 	p = input()
-
-
 
 	if (('run' in p) or ('open' in p)) and (('browser' in p) or ('chrome' in p)):
 		os.system("start chrome")
@@ -22,30 +17,23 @@ while True:
 	elif (('run' in p) or ('open' in p) or ('play' in p)) and (('media' in p) or ('music' in p) or ('audio player' in p)):
 		os.system("start wmplayer")
 
-
 	elif (('run' in p) or ('open' in p) or ('play' in p)) and (('calculator' in p) or ('calc' in p) or ('calculation' in p)):
 		os.system("start calc")
-
 
 	elif (('run' in p) or ('open' in p) or ('play' in p)) and (('paint' in p) or ('mspaint' in p) or ('draw' in p)):
 		os.system("start mspaint")
 
-
 	elif (('run' in p) or ('open' in p) or ('play' in p)) and (('excel' in p) or ('spreadsheet' in p) or ('office excel' in p)):
 		os.system("start excel")
 
-
 	elif (('run' in p) or ('open' in p) or ('play' in p)) and (('word' in p) or ('document editor' in p) or ('doc editor' in p)):
 		os.system("start winword")
-
 
 	elif (('run' in p) or ('open' in p) or ('play' in p)) and (('snipping' in p) or ('cutting tool' in p)):
 		os.system("start SnippingTool")
  
 	elif ('exit' in p) or ('quit' in p) or ('come out' in p ) or ('escape' in p):
 		break
-
-	
 
 	else:
 		print(" Unable to support your request ")
