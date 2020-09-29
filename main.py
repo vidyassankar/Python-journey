938ec4bdaf04a5b680f0603ed62539cc597ba906
Vidya <41564051+vidyassankar@users.noreply.github.com>
2020-09-29 22:19:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ While True:
 		print("Welcome ! , What are you looking for today? : ", end='')
 		p = input()
 		if ("date" in p):
-		 wb.open("http://172.27.173.58/cgi-bin/local.py?x=date")
+		 wb.open("http://172.27.173.58/cgi-bin/cgi.py?x=date")
 		elif("calendar" in p):
-		 wb.open("http://172.27.173.58/cgi-bin/local.py?x=cal")
+		 wb.open("http://172.27.173.58/cgi-bin/cgi.py?x=cal")
 		elif ("disk space" in p):
-		 wb.open("http://172.27.173.58/cgi-bin/local.py?x=df%20-k")
+		 wb.open("http://172.27.173.58/cgi-bin/cgi.py?x=df%20-k")
 		elif (("install" in p) and ("httpd")):
-		 wb.open("http://172.27.173.58/cgi-bin/local.py?x=yum%20install%20httpd")
+		 wb.open("http://172.27.173.58/cgi-bin/cgi.py?x=yum%20install%20httpd")
 		elif (("exit" in p) or ("quit" in p)):
 		 break
 		else:
